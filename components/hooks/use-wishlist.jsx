b1bd897730794d7476bc795373ef51279ae14e2c
Mmesoma Saint <mmesomasaint@gmail.com>
2022-10-21 11:18:41 +0100

    Fix: 'undefined is not a valid JSON'

--- components/hooks/use-wishlist.jsx
+++ components/hooks/use-wishlist.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { setCookie, getCookie } from 'cookies-next'
-import { useContext } from 'react'
 
 const WishListContext = React.createContext()
 
 export default function useWishList() {
-  const {list, count, addToList, removeFromList, inList} = useContext(WishListContext)
+  const { list, count, addToList, removeFromList, inList } =
+    useContext(WishListContext)
 
   return {
-    count, 
-    list, 
-    addToList, 
-    removeFromList, 
-    inList
+    count,
+    list,
+    addToList,
+    removeFromList,
+    inList,
   }
 }
 
@@ -26,14 +26,14 @@ export function WishListProvider({ children }) {
   }, [list])
 
   const addToList = (id) => {
-    const prevList = JSON.parse(getCookie('list'))
+    const prevList = JSON.parse(getCookie('list') || '[]')
     if (prevList.includes(id)) return
 
     setList([...prevList, id])
   }
 
   const removeFromList = (id) => {
-    const prevList = JSON.parse(getCookie('list'))
+    const prevList = JSON.parse(getCookie('list') || '[]')
     const idx = prevList.indexOf(id)
     if (idx === -1) return
 
@@ -44,7 +44,6 @@ export function WishListProvider({ children }) {
     const prevList = JSON.parse(getCookie('list') || '[]')
     return prevList.includes(id)
   }
-
 
   return (
     <WishListContext.Provider
